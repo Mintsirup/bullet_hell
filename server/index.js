fcb207684256d130460563902e6eb8f84d2155c4
TRX-16 <unicra2012@gmail.com>
2026-06-16 23:29:07 +0900

    version 1.0.0

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-import { wss }
-from "./socket.js";
+import { wss } from "./socket.js";
 import { fileURLToPath } from "url";
 
 import verifyReplay from "./verifyReplay.js";
